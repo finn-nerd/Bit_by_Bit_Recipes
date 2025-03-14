2df6fddd7dd1e7d80e4d45051990a026d582880a
Shiner <sjs.shiner@gmail.com>
2025-03-14 12:03:40 -0400

    PostgreSQL integration

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -2,6 +2,25 @@
 var express = require('express');
 var router = express.Router();
 
+// PostgreSQL database connection
+const {Client} = require('pg')
+const client = new Client({
+    user: "postgres",
+    password: "newpassword",
+    host: "localhost",
+    database: "users",
+})
+
+connect();
+async function connect() {
+    try {
+        await client.connect()
+        console.log("Connected to the database.");
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+    }
+}
+
 // when a GET request is made to '/', the server will respond with the message
 router.get('/', function(req, res, next) {
     res.send('API is working properly');
