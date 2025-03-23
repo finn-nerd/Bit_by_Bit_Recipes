6d950bd59c2b07e1929e40e6e41d7e8bc0422476
finn-nerd <walker.finn123@gmail.com>
2025-03-22 19:07:25 -0500

    deployment test, removed problematic code

--- api/app.js
+++ api/app.js
@@ -16,10 +16,6 @@ var app = express();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
