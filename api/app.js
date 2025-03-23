e536622223c737672b3a0fefa1cf2c6e6c3f9ec7
finn-nerd <walker.finn123@gmail.com>
2025-03-22 21:08:55 -0500

    fix failsafe catcher catching everything, deployment test

--- api/app.js
+++ api/app.js
@@ -23,28 +23,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// service frontend build
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // route handlers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-// catch-all route to handle all frontend requests (important for React Router)
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
-
 // test the API
 app.use('/testAPI', testAPIRouter);
 
 // test client side
-// app.use(express.static(path.join(__dirname, '../client/build')));
 app.post('/client-side-test', (req, res) => {
     const data = req.body.input;
     console.log('Received:', data);
     res.json({ message: "SERVER RECEIVED: " + data});
 });
+
+// catch-all route to handle all missed frontend requests
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+ });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
