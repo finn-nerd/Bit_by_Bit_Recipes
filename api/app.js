82bd8a3f5642038f83febb0293de4edacaafe549
finn-nerd <walker.finn123@gmail.com>
2025-03-23 00:14:33 -0500

    specified CORS methods

--- api/app.js
+++ api/app.js
@@ -18,7 +18,9 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 
-app.use(cors());
+app.use(cors({
+    methods: ['GET', 'POST', 'OPTIONS']
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
