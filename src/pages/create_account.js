9f7360ef9e58ad807a346a9b58993c78346148a7
linsaraj <linsaraj0@gmail.com>
2025-03-24 19:24:42 -0400

    updated pizza positions

--- src/pages/create_account.js
+++ src/pages/create_account.js
@@ -39,8 +39,8 @@ function CreateAccount() {
     return (
         <div className="App">
             {/* floating pizza */}
-            <img className="floating pizza" style={{top: '50px', left: '550px'}} src="/pizza.png" alt="Pizza" />
-            <img className="floating pizza" style={{top: '50px', right: '550px'}} src="/pizza.png" alt="Pizza" />
+            <img className="floating pizza" style={{top: '50px', left: '500px'}} src="/pizza.png" alt="Pizza" />
+            <img className="floating pizza" style={{top: '50px', right: '500px'}} src="/pizza.png" alt="Pizza" />
 
             {/* blocks for background aesthetics */}
             <div className="red-block" style={{top: '0px', left: '0px'}}></div>
