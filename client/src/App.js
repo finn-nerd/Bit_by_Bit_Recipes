c75e4c74eafde5c415e49b1bf3cb99a2e9c6e174
linsaraj <linsaraj0@gmail.com>
2025-03-23 22:05:47 -0400

    implemented switching between diff pages
    updated the versions of react so that react-dom-router could work
    created a second page and can switch between them by clicking the button

--- client/src/App.js
+++ client/src/App.js
@@ -1,92 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import LoginPage from './LoginPage';
+import CreateAcct from './CreateAcct';
 
 // set routing api, or let vercel handle local routing
 const apiUrl = process.env.REACT_APP_API_URL || "";
 
 function App() {
-
-  // creates state variables
-  const [apiResponse, setApiResponse] = useState('');
-  const [userInput, setUserInput] = useState('');
-  const [userInputResponse, setUserInputResponse] = useState('');
-
-  // runs function once and makes a GET request to the following link
-  useEffect(() => {
-    fetch(`${apiUrl}/api/testAPI`)
-      // converts response into plain text
-      .then((res) => res.text())
-      // stores response in variable
-      .then((res) => setApiResponse(res));
-  }, []);
-
-  // send input to backend/api
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-    // send to server and record response
-    const response = await fetch(`${apiUrl}/api/client-side-test`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ input: userInput })
-    });
-
-    // output response on frontend
-    const data = await response.json(); // parse json stream into object
-    setUserInputResponse(data.message); // extract json "message" field
-    } catch (err) {
-      console.error("Error during fetch:", err);
-      setUserInputResponse("An error occurred while contacting the server.");
-    }
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* title for homepage */}
-        <div className="text-wrapper">
-          <h1 className="front-title">Bit by Bit Recipes</h1>
-          <h1 className="back-title">Bit by Bit Recipes</h1>
-        </div>
-
-        {/* overall text box */}
-        <div className="text-box">
-          {/* welcome box and text */}
-          <div className="welcome-box">
-            <p className="welcome-text">Welcome! Please enter your username and password! (:</p>
-          </div>
-          
-          {/* username title and input box */}
-          <p className="user-pass-text">Username</p> 
-          <input 
-            type="text"
-            value={userInput} // contains user input
-            onChange={(e) => setUserInput(e.target.value)} 
-            className="input-field"
-            placeholder="Enter your username"
-          />
-
-          {/* password title and input box */}
-          <p className="user-pass-text">Password</p>
-          <input
-            className="input-field"
-            placeholder="Enter your password"
-          />
-        </div>
-
-        {/* submit button */}
-        <form className="form-box" onSubmit={handleSubmit}>
-          <button className="button-box" type="submit">
-            Submit
-          </button>
-        </form>
-
-        {/* outputs success or not for user authentication */}
-        <h1>User Authentication: {userInputResponse}</h1>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* routes for diff pages */}
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/create_account" element={<CreateAcct />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/CreateAcct.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function CreateAcct() {
+
+// variables
+const navigate = useNavigate(); // used to navigate to diff pages
+
+// used to navigate to diff page
+const handleClick = () => navigate('/');
+
+return (
+    <div className="App">
+        <header className="App-header">
+            {/* title for page */}
+            <div className="text-wrapper">
+                <h1 className="front-title">Bit by Bit Recipes</h1>
+                <h1 className="back-title">Bit by Bit Recipes</h1>
+            </div>
+
+            {/* overall text box */}
+            <div className="text-box">
+                {/* welcome box and text */}
+                <div className="welcome-box">
+                    <p className="welcome-text">create acct</p>
+                </div>
+                
+                {/* username title and input box */}
+                <p className="user-pass-text">Username</p> 
+                <input 
+                className="input-field"
+                placeholder="Enter your username"
+                />
+
+                {/* password title and input box */}
+                <p className="user-pass-text">Password</p>
+                <input
+                className="input-field"
+                placeholder="Enter your password"
+                />
+            </div>
+
+            {/* submit button to check user authentication*/}
+            <form className="form-box">
+                <button className="button-box" type="submit" onClick={handleClick}>
+                    Submit
+                </button>
+            </form>
+        </header>
+    </div>
+);
+
+
+}
+
+export default CreateAcct;
