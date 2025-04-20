2365cb14d9333c800d18abeac6d03e5927610dcd
linsaraj <linsaraj0@gmail.com>
2025-04-19 23:59:41 -0400

    filter functionality
    filter frontend and backend for categories, areas, and an ingredient

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,51 +1,72 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Sidebar from '../../components/sidebar'
+import { Listbox } from '@headlessui/react';
 
 function Home() {
   const router = useRouter();
-  const [search_input, setSearch_Input] = useState('');
   const [userInputResponse, setUserInputResponse] = useState('');
-  const [meals, setMeals] = useState([]);
+
+  // search bar
+  const [search_input, setSearch_Input] = useState(''); // search bar input
+
+  // fetch meals
+  const [meals, setMeals] = useState([]); // meal cards
   const [loading, setLoading] = useState(false);
+
+  // category dropdown menu
+  const [categories, setCategories] = useState([]);
+  const [selectedCat, setSelectedCat] = useState(''); 
+
+  // area dropdown menu
+  const [areas, setAreas] = useState([]);
+  const [selectedArea, setSelectedArea] = useState(''); 
+
+  // ingredient search bar
+  const [search_ing, setSearch_Ing] = useState('');
+
+  // side bar
   const [openSidebar, setOpenSidebar] = useState(false); // sidebar isnt open by default
 
   // Create list of favorited meals
   const [savedMeals, setSavedMeals] = useState([]); // or useState(new Set())
 
-    useEffect(() => {
-        fetchSavedMeals();
-    }, []);
 
-    // Find currently favorited meals
-    const fetchSavedMeals = async () => {
-        // TODO:
-        // find all favorite meal ids
-        // populate the 'favorites' list with it
+  useEffect(() => {
+    fetchMeals(); // default category
+    fetchSavedMeals();
+    fetchCats();
+    fetchAreas();
+  }, []);
 
-        // dummy data for now
-        setSavedMeals(['52772', '52874', '52913']);
-    };
+  // Favorite a meal
+  const toggleSavedMeal = (mealID) => {
+      const isNowSaved = !savedMeals.includes(mealID)
+      setSavedMeals((prev) =>
+          isNowSaved ? [...prev, mealID] : prev.filter((id) => id !== mealID)
+      );
 
-    // Favorite a meal
-    const toggleSavedMeal = (mealID) => {
-        const isNowSaved = !savedMeals.includes(mealID)
-        setSavedMeals((prev) =>
-            isNowSaved ? [...prev, mealID] : prev.filter((id) => id !== mealID)
-        );
+      console.log(`SAVED ${mealID}: ${isNowSaved}`);
+      // TODO:
+      // update backend copy of favorites list
+      // if (isNowFav) --> add it to db
+      // else --> delete it from db
+  };
+  // Find currently favorited meals
+  const fetchSavedMeals = async () => {
+      // TODO:
+      // find all favorite meal ids
+      // populate the 'favorites' list with it
 
-        console.log(`SAVED ${mealID}: ${isNowSaved}`);
-        // TODO:
-        // update backend copy of favorites list
-        // if (isNowFav) --> add it to db
-        // else --> delete it from db
-    };
+      // dummy data for now
+      setSavedMeals(['52772', '52874', '52913']);
+  };
 
   // Fetch meal data from backend
-  const fetchMeals = async (query = 'chicken') => {
+  const fetchMeals = async (query = 'chicken', type='search') => {
     setLoading(true);
     try {
-      const res = await fetch(`/api/meals?recipe=${query}`);
+      const res = await fetch(`/api/meals?recipe=${encodeURIComponent(query)}&type=${type}`);
 
       // if category doesn't exist
       if (!res.ok) {
@@ -65,19 +86,62 @@ function Home() {
     setLoading(false);
   };
 
-  useEffect(() => {
-    fetchMeals(); // default category
-  }, []);
+  // Fetch categories for dropdown menu
+  const fetchCats = async (query) => {
+    try {
+      const res = await fetch(`/api/categories`);
+      const data = await res.json();
 
+      const categoryNames = data.meals.map((cat) => cat.strCategory);
+      setCategories(categoryNames);
+    } catch (err) {
+      console.error("Failed to categories:", err);
+      setCategories([]);
+    }
+  };
+  // for category selection from filter bar
+  const handleSelectCat = (value) => {
+    setSelectedCat(value)
+    fetchMeals(value, 'category');
+  }
+
+  // Fetch categories for dropdown menu
+  const fetchAreas = async (query) => {
+    try {
+      const res = await fetch(`/api/areas`);
+      const data = await res.json();
+
+      const areaNames = data.meals.map((area) => area.strArea);
+      setAreas(areaNames);
+    } catch (err) {
+      console.error("Failed to areas:", err);
+      setAreas([]);
+    }
+  };
+  // for area selection from filter bar
+  const handleSelectArea = (value) => {
+    setSelectedArea(value)
+    fetchMeals(value, 'area');
+  }
+
+  // search bar enter
   const handleEnter = async (e) => {
     e.preventDefault();
-    fetchMeals(search_input);
+    fetchMeals(search_input, 'search');
   };
 
+  // search bar enter for ingredients
+  const handleEnterIng = async (e) => {
+    e.preventDefault();
+    fetchMeals(search_ing, 'ingredient');
+  };
+
+  // clicking on a meal card
   const handleRedirect = (recipeID) => {
     router.push(`/recipes/${recipeID}`);
   }
 
+  // sidebar
   const toggleSidebar = () => setOpenSidebar(!openSidebar);
   const closeSidebar = () => setOpenSidebar(false);
 
@@ -85,7 +149,82 @@ function Home() {
     <div className="App">
 
       {/* Left filter bar */}
-      <div className="fixed left-0 w-[10%] h-[100%] bg-gradient-to-b from-[#EEAE36] to-[#E97832] z-1"></div>
+      <div className="fixed overflow-y-auto scrollbar-hide left-0 w-[15%] top-[10%] h-[90%] bg-gradient-to-b from-[#EEAE36] to-[#E97832] z-1 flex flex-col items-center">
+        <p className="text-xl sm:text-2xl xl:text-[25px] text-black pt-10 font-['Jersey_10']">Choose a Category: </p>
+
+        {/* Dropdown menu for categories */}
+        <Listbox
+        as='div'
+        value={selectedCat}
+        onChange={handleSelectCat}>
+
+          {/* Dropdown button */}
+          <Listbox.Button className="w-full cursor-pointer text-xl sm:text-2xl xl:text-[22px] text-black py-3 focus:outline-none opacity-[60%] font-['Jersey_10']">
+            {selectedCat || 'Select an option:'}
+          </Listbox.Button>
+
+          {/* Dropdown options */}
+          <Listbox.Options>
+            {categories.map((category, index) => (
+              <Listbox.Option
+              key={index}
+              value={category}
+              className={({active}) =>
+                `cursor-pointer select-none p-2 ${active ? 'bg-[#EF6F34] text-black' : ''}`}
+              >
+                {category}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+          
+        </Listbox>
+
+        <p className="text-xl sm:text-2xl xl:text-[25px] text-black pt-5 font-['Jersey_10']">Choose an Area: </p>
+
+        {/* Dropdown menu for area */}
+        <Listbox
+        as='div'
+        value={selectedArea}
+        onChange={handleSelectArea}>
+
+          {/* Dropdown button */}
+          <Listbox.Button className="w-full cursor-pointer text-xl sm:text-2xl xl:text-[22px] text-black py-3 focus:outline-none opacity-[60%] font-['Jersey_10']">
+            {selectedArea || 'Select an option:'}
+          </Listbox.Button>
+
+          {/* Dropdown options */}
+          <Listbox.Options>
+            {areas.map((area, index) => (
+              <Listbox.Option
+              key={index}
+              value={area}
+              className={({active}) =>
+                `cursor-pointer select-none p-2 ${active ? 'bg-[#EF6F34] text-black' : ''}`}
+              >
+                {area}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+          
+        </Listbox>
+
+        <p className="text-xl sm:text-2xl xl:text-[25px] text-black pt-5 font-['Jersey_10']">Choose an Ingredient: </p>
+
+        {/* Search bar for ingredients */}
+        <input
+            type="text"
+            value={search_ing}
+            onChange={(e) => setSearch_Ing(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleEnterIng(e);
+              }
+            }}
+            className="relative bg-[#E65340] text-black text-base sm:text-lg md:text-xl lg:text-[20px] w-[80%] rounded-[25px] border-[4px] border-[#C13737] mt-3 mb-7 p-3 font-['Jersey_10']"
+            placeholder="Search for an ingredient..."
+          />
+
+      </div>
 
       {/* Top bar */}
       <div className="fixed top-0 left-0 w-full bg-gradient-to-b from-[#F18D5E] to-[#EF6F34] z-10">
@@ -114,7 +253,7 @@ function Home() {
       </div>
 
       {/* Meal Cards */}
-      <div className="flex flex-wrap justify-center mt-[180px] px-10 pb-10 pl-[12%] gap-10">
+      <div className="flex flex-wrap justify-center mt-[180px] pb-10 pl-[16%] gap-7">
 
         {loading ? (
           <p className="text-white text-2xl font-bold">Loading...</p>
