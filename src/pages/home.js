3abf532a95240d39e5fc556b30b815557854a02e
finn-nerd <walker.finn123@gmail.com>
2025-04-20 22:12:09 -0400

    Fixed broken merge

--- src/pages/home.js
+++ src/pages/home.js
@@ -399,7 +399,14 @@ function Home({ isLoggedIn }) {
                           e.stopPropagation(); // Prevent triggering onClick of the card
                           toggleSavedMeal(meal);  // Send meal to backend to be favorited/unfavorited
                       }}
-                      className="cursor-pointer absolute top-2 right-2 transform -translate-y-full opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-200" />
+                      className="cursor-pointer absolute top-2 right-2 transform -translate-y-full opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-200"
+                  >
+                      <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      className={`w-6 h-6 text-yellow-400 ${isSaved ? 'fill-current' : 'fill-none'} stroke-current stroke-2`}
+                      >
+                      
                       <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
                       </svg>
                   </button>
